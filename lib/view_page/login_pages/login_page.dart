01e750b7f8f28dc1f361d388059d882c4c9c417e
Asim-9812 <asim981201@gmail.com>
2023-03-10 01:45:03 +0545

    first commit

---
+++ lib/view_page/login_pages/login_page.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class LoginPage extends StatefulWidget {
+
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+
+  
+}
